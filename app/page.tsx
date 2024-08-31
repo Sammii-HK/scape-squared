7b16a986ea774fa9ccc39c79fd384fb653e1b13b
Sammii <kellow.sammii@gmail.com>
2024-08-31 07:21:53 +0100

    homepage design overhaul

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Footer from 'components/layout/footer';
+import { HeroCarousel } from 'components/layout/hero-carousel';
 import { Carousel } from 'components/ui/carousel';
+import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export const runtime = 'edge';
@@ -11,28 +14,50 @@ export const metadata = {
   },
 };
 
+const aboutUs = [
+  ["100% organic cotton", "garments, always"],
+  ["Made to order,", "just for you"],
+  ["Unique designs,", "to stand out"],
+];
+
+const CollectionCarousel = ({collection}: {collection: "flowerscapes" | "foliagescapes"}) => {
+  const src = {
+    flowerscapes: "/flowerscape-2.png",
+    foliagescapes: "/foliagescape-1.jpg",
+  }
+  return (
+    <Link href={`collection/${collection}`} className='col-span-1 cursor-pointer'>
+      <Image width="640" height="640" className="w-full" alt={collection} src={src[collection]} />
+      <div className="flex mb-5 pt-2 justify-between align-middle">
+        <h5 className='text-3xl capitalize'>{collection}</h5>
+        <p className='text-sm font-bold justify-end self-center'>SHOP NOW</p>
+      </div>
+    </Link>
+  )
+}
+
 export default async function HomePage() {
   return (
     <>
       <Suspense>
 
         <div className='container grid justify-around my-4 grid-cols-1 items-center justify-items-center'>
+          <HeroCarousel />
+          <div className="grid grid-cols-1 md:grid-cols-3 my-8 w-full">
+            {aboutUs.map((item, index) => (
+              <div className='flex align-middle justify-center flex-col py-7 text-xl' key={`aboutUs-item-${index}`}>
+                <p className="font-bold col-span-1 w-full justify-center flex">{item[0]}</p>
+                <p className="font-bold col-span-1 w-full justify-center flex">{item[1]}</p>
+              </div>
+            )
+            )}
+          </div>
           <Carousel collection={undefined} />
-          <Carousel collection='flower' />
-          <Carousel collection='foliage' />
-          <Carousel collection='nature' />
-          {/* <Link className='h-full w-full md:w-10/12 flex items-center justify-center justify-items-center' href="/wall">
-            <Button className='py-3 w-full m-3 bg-transparent' variant={'dark'}>
-              <h2 className='absolute z-50 text-3xl text-white'>Art for the Wall</h2>
-              <GridTileImage width={500} height={500} src="/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0587%2F3251%2F1370%2Fproducts%2F2f6e3c70-ed36-4034-b670-7eda7d47afe1.jpg%3Fv%3D1700689495&w=1080&q=75" alt='Art for the Wall' />
-            </Button>
-          </Link>
-          <Link className='h-full w-full md:w-10/12 flex items-center justify-center' href="/wardrobe">
-            <Button className='py-3 w-full m-3 bg-transparent' variant={'dark'}>
-              <h2 className='absolute z-50 text-3xl text-white'>Art for the Wardrobe</h2>
-              <GridTileImage width={500} height={500} src="/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0587%2F3251%2F1370%2Ffiles%2F2BCB00FC-35D9-4181-8F11-D6F4569D6446.jpg%3Fv%3D1700746166&w=1080&q=75" alt='Art for the Wardrobe' />
-            </Button>
-          </Link> */}
+
+          <div className="grid grid-cols-1 md:grid-cols-2 justify-between gap-10 my-8">
+            <CollectionCarousel collection='flowerscapes' />
+            <CollectionCarousel collection='foliagescapes' />
+          </div>
 
         </div>
       </Suspense>
