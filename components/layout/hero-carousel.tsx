b64adda15ca06791164fb4cdc85d6944836097ca
Sammii <kellow.sammii@gmail.com>
2024-08-31 07:18:32 +0100

    Hero carousel component

---
+++ components/layout/hero-carousel.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const carouselItems = [
+  {
+    key: "art-prints",
+    alt: "",
+  },
+  {
+    key: "t-shirts",
+    alt: "",
+  },
+];
+
+export const HeroCarousel = () => {
+  const [activeCarouselItem, setActiveCarouselItem] = useState(carouselItems[0]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      activeCarouselItem!.key === "art-prints" ? setActiveCarouselItem(carouselItems[1]) : setActiveCarouselItem(carouselItems[0]);
+    }, 5000)
+  }, [activeCarouselItem])
+
+  return (
+    <Link className='cursor-pointer' href={`/collection/${activeCarouselItem!.key}`}>
+      <Image className='md:hidden' width="640" height="640" alt={activeCarouselItem!.alt} src={`/carousel/${activeCarouselItem!.key}.jpg`} />
+      <Image className='hidden md:block' width="1400" height="450" alt={activeCarouselItem!.alt} src={`/carousel/${activeCarouselItem!.key}-tablet.jpg`} />
+    </Link>
+  )
+}
