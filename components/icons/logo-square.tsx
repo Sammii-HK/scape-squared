4d04a58b11e00dbc6a69171890b5eff794a8e621
Sammii <kellow.sammii@gmail.com>
2024-09-05 10:56:20 +0100

    creating more logo icons, square & typography

--- components/icons/logo-square.tsx
+++ components/icons/logo-square.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function LogoIcon(props: React.ComponentProps<'img'>) {
+export default function LogoSquareIcon(props: React.ComponentProps<'img'>) {
   return (
     <Image width="100" height="50" src="/logo-square-black.png" alt="scape squared logo" />
   );
---
+++ components/icons/logo-typography.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function LogoTypographyIcon(props: React.ComponentProps<'img'>) {
+  return (
+    <Image width="100" height="50" src="/logo-white.png" alt="scape squared logo" />
+  );
+}
