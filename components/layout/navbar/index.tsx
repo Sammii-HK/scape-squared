1c85be304c655fe885819e4602add0ad9efba928
Sammii <kellow.sammii@gmail.com>
2024-09-05 12:55:03 +0100

    adding cart and search to nav

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,7 +1,11 @@
+import Cart from "@/components/cart";
+import OpenCart from "@/components/cart/open-cart";
 import LogoTypographyIcon from "@/components/icons/logo-typography";
 import Link from "next/link";
+import { Suspense } from "react";
 import DesktopMenu from "./desktop-menu";
 import MobileMenu from './mobile-menu';
+import Search from "./search";
 
 export default async function Navbar() {
   return (
@@ -9,10 +13,17 @@ export default async function Navbar() {
       <div className="block flex-none md:hidden">
         <MobileMenu />
       </div>
+      {/* socials ? */}
       <div className="flex w-full justify-center md:pb-4">
         <Link href="/">
           <LogoTypographyIcon />
         </Link>
+        <div className="absolute flex right-0 align-middle">
+          <Search />
+          <Suspense fallback={<OpenCart />}>
+            <Cart />
+          </Suspense>
+        </div>
       </div>
       <div className="w-full items-center justify-center hidden md:flex">
         <DesktopMenu />
