f740684648f32a5f4e979db9583f17493af76dae
Sammii <kellow.sammii@gmail.com>
2024-09-05 10:15:18 +0100

    navbar/index

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,15 +1,15 @@
 // import { getMenu } from 'lib/shopify';
-import { menu } from "../../../constants/menu";
+import DesktopMenu from "./desktop-menu";
 import MobileMenu from './mobile-menu';
 
 export default async function Navbar() {
   return (
-    <nav className="relative flex items-center justify-between p-4 lg:px-6 bg-black">
+    <nav className="relative flex items-center justify-between p-4 lg:px-6 bg-neutral-900">
       <div className="block flex-none md:hidden">
-        <MobileMenu menu={menu} />
+        <MobileMenu />
       </div>
-      <div className="flex w-full items-center">
-       {/* <DesktopMenu menu={menu} /> */}
+      <div className="w-full items-center justify-center hidden md:flex">
+        <DesktopMenu />
       </div>
     </nav>
   );
