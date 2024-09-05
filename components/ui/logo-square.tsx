4dbb6e60399b037ac165810a61acb181212282c4
Sammii <kellow.sammii@gmail.com>
2024-09-05 15:06:35 +0100

    .

--- components/ui/logo-square.tsx
+++ components/ui/logo-square.tsx
@@ -12,12 +12,7 @@ export default function LogoSquare({ size }: { size?: 'sm' | undefined }) {
         }
       )}
     >
-      <LogoSquareIcon
-        className={clsx({
-          'h-[16px] w-[16px]': !size,
-          'h-[10px] w-[10px]': size === 'sm'
-        })}
-      />
+      <LogoSquareIcon />
     </div>
   );
 }
