772b861d56fbd6dbc66914fd5007acba162f7194
Sammii <kellow.sammii@gmail.com>
2024-12-28 11:55:53 +0000

    adding speed insights to site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Navbar from 'components/layout/navbar';
 import { Inter } from 'next/font/google';
 import { ReactNode, Suspense } from 'react';
@@ -36,11 +37,12 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
     <html lang="en" className={inter.variable}>
       <meta name="google-site-verification" content="nU-MGbKTYYrFX-Yh7SIgQ1szTEoYTF08PZU1nihRS6g" />
       <body className="bg-neutral-50 text-neutral-500">
-        <Analytics />
         <Navbar />
         <Suspense>
           <main>{children}</main>
         </Suspense>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
